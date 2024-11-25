2cf84ce1c8b81c76037827aefe7ab9562dce4d6d
Jan <150939827+Peny835@users.noreply.github.com>
2024-11-25 09:11:55 +0100

    Update pin.cpp

--- pin.cpp
+++ pin.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <windows.h>
 
 using namespace std;
 
@@ -11,36 +10,21 @@ int main() {
     string wprowadzonyPin;
 
     while (proby < maxProb) {
-        cout << "-------------------\033[35mTELEFON\033[0m-----------------" << endl;
-        cout << "\033[34mWprowadz PIN: ";
+        cout << "-------------------TELEFON-----------------" << endl;
+        cout << "Wprowadz PIN: ";
         cin >> wprowadzonyPin;
 
         if (wprowadzonyPin == poprawnyPin) {
-            cout << "\033[32mTelefon odblokowany!\033[0m" << endl;
+            cout << "Telefon odblokowany!" << endl;
             return 0;
         } else {
             proby++;
-            switch(proby) {
-                case 1:
-                    cout << "\033[32mNiepoprawny PIN. Pozostalo 4 proby.\033[0m" << endl;
-                    break;
-                case 2:
-                    cout << "\033[33mNiepoprawny PIN. Pozostalo 3 proby.\033[0m" << endl;
-                    break;
-                case 3:
-                    cout << "\033[93mNiepoprawny PIN. Pozostalo 2 proby.\033[0m" << endl;
-                    break;
-                case 4:
-                    cout << "\033[91mNiepoprawny PIN. Pozostala 1 proba.\033[0m" << endl;
-                    break;
-            }
+            cout << "Niepoprawny kod pin pozostało prob: " << maxProb - proby <<endl;
         }
     }
 
-    cout << "\033[31mZbyt wiele niepoprawnych prob. Telefon zablokowany na 15 minut.\033[0m" << endl;
-    cout << "\033[33mSprobuj ponownie za 15 minut.\033[0m" << endl;
-
-    Sleep(900000); // 15 minut
+    cout << "Zbyt wiele niepoprawnych prob. Telefon zablokowany na 15 minut." << endl;
+    cout << "Sprobuj ponownie za 15 minut." << endl;
 
     return 0;
 }
